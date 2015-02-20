0c1087773fe80a3e322eaf39419f3c7ad76450ba
Anthony Calandra <anthony@anthony-calandra.com>
2015-02-20 13:19:47 -0500

    Fixed draft bug. Fixed inconsistent formatting.

--- config.js
+++ config.js
@@ -1,38 +1,38 @@
 module.exports = {
-	// Site config.
-	siteTitle: 'Welcome to NodeBlog!',
-	siteDesc: 'A blog about things.',
+  // Site config.
+  siteTitle: 'Welcome to NodeBlog!',
+  siteDesc: 'A blog about things.',
 
-	// Category config.
-	// Each element of the array is a string which is the category name.
-	// Category ID's are mapped to this array. Thus, there should be ATLEAST ONE
-	// element in this array at all times.
-	categories: ['Uncategorized', 'General'],
+  // Category config.
+  // Each element of the array is a string which is the category name.
+  // Category ID's are mapped to this array. Thus, there should be ATLEAST ONE
+  // element in this array at all times.
+  categories: ['Uncategorized', 'General'],
 
-	// Posts config.
-	maxPostLength: 10000,
-	minPostLength: 10,
-	maxTitleLength: 100,
-	minTitleLength: 3,
-	postPreviewLength: 100,
+  // Posts config.
+  maxPostLength: 10000,
+  minPostLength: 10,
+  maxTitleLength: 100,
+  minTitleLength: 3,
+  postPreviewLength: 100,
 
-	// Admin config.
-	admin: {
-		username: 'admin',
-		password: ''
-	},
+  // Admin config.
+  admin: {
+    username: 'admin',
+    password: ''
+  },
 
-	// Mongodb config.
-	// mongodb://username:password@host:port/database?options...
-	mongoConnStr: '',
-	mongoOpts: {
-		server: {
-			socketOptions: {
-				keepAlive: 1
-			}
-		}
-	},
+  // Mongodb config.
+  // mongodb://username:password@host:port/database?options...
+  mongoConnStr: '',
+  mongoOpts: {
+    server: {
+      socketOptions: {
+        keepAlive: 1
+      }
+    }
+  },
 
-	// Session config.
-	cookieSecret: ''
+  // Session config.
+  cookieSecret: ''
 };
